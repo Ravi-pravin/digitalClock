348b84e8ac9617b6f13a9097149726e795569970
Ravi <pravinravi.p03@gmail.com>
2026-03-22 10:45:28 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const toggleBtn = document.getElementById("toggleMode");
+
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+const dateEl = document.getElementById("date");
+
+function updateClock(){
+    const now = new Date();
+
+    let h = now.getHours();
+    let m = now.getMinutes();
+    let s = now.getSeconds();
+
+    h = h % 12 || 12;
+
+    hoursEl.innerText = String(h).padStart(2,"0");
+    minutesEl.innerText = String(m).padStart(2,"0");
+    secondsEl.innerText = String(s).padStart(2,"0");
+
+    dateEl.innerText = now.toDateString();
+}
+
+setInterval(updateClock,1000);
+updateClock();
+
+toggleBtn.addEventListener("click",()=>{
+
+    document.body.classList.toggle("dark");
+
+    const icon = toggleBtn.querySelector("i");
+
+    icon.classList.toggle("fa-moon");
+    icon.classList.toggle("fa-sun");
+
+});
