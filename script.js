bea62996b1702b4b1608806a8ab98c206c0e990b
Ravi <pravinravi.p03@gmail.com>
2026-05-14 12:25:13 +0530

    2nd commit

--- script.js
+++ script.js
@@ -4,33 +4,34 @@ const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 const dateEl = document.getElementById("date");
+const ampmEl = document.getElementById("ampm");
 
-function updateClock(){
-    const now = new Date();
+function updateClock() {
+  const now = new Date();
 
-    let h = now.getHours();
-    let m = now.getMinutes();
-    let s = now.getSeconds();
+  let h = now.getHours();
+  let m = now.getMinutes();
+  let s = now.getSeconds();
 
-    h = h % 12 || 12;
+  let ampm = h >= 12 ? "PM" : "AM";
 
-    hoursEl.innerText = String(h).padStart(2,"0");
-    minutesEl.innerText = String(m).padStart(2,"0");
-    secondsEl.innerText = String(s).padStart(2,"0");
+  h = h % 12 || 12;
 
-    dateEl.innerText = now.toDateString();
+  hoursEl.innerText = String(h).padStart(2, "0");
+  minutesEl.innerText = String(m).padStart(2, "0");
+  secondsEl.innerText = String(s).padStart(2, "0");
+  ampmEl.innerText = ampm;
+
+  dateEl.innerText = now.toDateString();
 }
 
-setInterval(updateClock,1000);
+setInterval(updateClock, 1000);
 updateClock();
 
-toggleBtn.addEventListener("click",()=>{
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
 
-    document.body.classList.toggle("dark");
-
-    const icon = toggleBtn.querySelector("i");
-
-    icon.classList.toggle("fa-moon");
-    icon.classList.toggle("fa-sun");
-
+  const icon = toggleBtn.querySelector("i");
+  icon.classList.toggle("fa-moon");
+  icon.classList.toggle("fa-sun");
 });
